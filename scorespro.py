78664006009dcbf3b29429399977976a463e54c4
Meerxn <fardeenfmeeran@gmail.com>
2020-07-06 22:02:59 +0400

    commented code for fardeen please do not touch

--- scorespro.py
+++ scorespro.py
@@ -1,5 +1,3 @@
-# Scrapping soccer scores from scorespro.com : https://www.scorespro.com/
-
 import urllib.request as urllib2
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
@@ -9,6 +7,7 @@ from datetime import datetime, date
 
 # Use this as url for cli but going to hardcode it for now
 url = 'https://www.scorespro.com/'
+url2 = 'https://www.scorespro.com/basketball/'
 # url = str(sys.argv[1])
 
 def soup_it(url):
@@ -78,9 +77,73 @@ def find_data(url):
             main_sports.append([leauge_name, home, score, away, match_kick_str])
 
     return main_sports
+"""  
+def find_data_basket(url):
+    soup = soup_it(url)
+    # Looks in at the mainfeed
+    main_feed = soup.find('div', id="mainfeed")    
+    # gets all the comp groups 
+    comp_groups = main_feed.find_all('div', class_='compgrp')
+    # Find Leauge Data in compgroups
+    main_sports = []
+  
+    for leauge in comp_groups:
+        
+        leauge_name = ''
+        kick_off_time = ''
 
-
-    
+        l_data = leauge.find(lambda tag: tag.name=='td', class_ ="blockBarLt")
+        for a in l_data.find_all('a'):
+            leauge_name += a.text
+            print(leauge_name)
+        
+        matches_in_leauges = leauge.find_all('table')
+        print(matches_in_leauges)
+        for match in matches_in_leauges:
+         match_kick = (match.find(lambda tag: tag.name=='td', class_ ="datetime"))
+         match_kick.text
+         
+         
+         """ 
+       
+         status_td = match.find(lambda tag: tag.name=='td', class_ ="status")
+  
+         if status_td != None:
+            status = status_td.find(lambda tag: tag.name == 'span')
+            status = status.text
+         else:
+             status = "NONE"
+            
+       
+         if status == None: 
+             status = "None"
+         else:
+      
+          
+             
+         home_uc = match.find(lambda tag: tag.name=='td', class_ ="h_n hometown")
+         home = home_uc.find(lambda tag: tag.name == 'a')
+         if home == None or len(home) == 0: 
+                home = "None"
+         else:
+                home = home.text       
+         away_uc = match.find(lambda tag: tag.name=='td', class_ ="away")
+         away = away_uc.find(lambda tag: tag.name == 'a')
+         if away == None or len(away) == 0: 
+                away = "None"
+         else:
+                away = away.text
+         score_td = match.find(lambda tag: tag.name=='td', class_ ="total ts_set")
+         score = score_td.find(lambda tag: tag.name == 'a')
+         if score == None or len(score) == 0: 
+                score = "0 - 0"
+         else:
+                score = score.text
+         main_sports.append([leauge_name, home, score, away,match_kick])
+    return main_sports 
+"""        
+      
 
 print(find_data(url))
+print(find_data_basket(url2))
 
